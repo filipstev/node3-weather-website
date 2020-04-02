ea3d2097bc423708f4471706e8a5ff9721cb1611
Filip <filipstevanovic176@gmail.com>
2020-04-02 20:54:18 +0200

    added new features to forecast

--- src/app.js
+++ src/app.js
@@ -23,14 +23,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req,res) => {
     res.render('index' , {
         title:'Weather',
-        name: 'Andrew Mead'
+        name: 'Filip'
     })
 })
 
 app.get('/about', (req,res) => {
     res.render('about', {
         title:'About Me',
-        name:'Andrew Mead'
+        name:'Filip'
     })
 })
 
@@ -38,7 +38,7 @@ app.get('/help', (req,res) => {
     res.render('help', {
         title:'Help',
         helpText:'This is some helpful text',
-        name:'Andrew Mead'
+        name:'Filip'
     })
 })
 
@@ -83,7 +83,7 @@ app.get('/products',(req, res) => {
 app.get('/help/*' , (req,res) => {
     res.render('404', {
         title:'404',
-        name:'Andrew Mead',
+        name:'Filip',
         errorMessage:'Help article not found'
     })
 })
@@ -91,7 +91,7 @@ app.get('/help/*' , (req,res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title:'404',
-        name:'Andrew Mead',
+        name:'Filip',
         errorMessage:'Page not found'
     })
 })
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request')
 
+
+
 const forecast = (latitude, longitude,callback) => {
     const url = 'https://api.darksky.net/forecast/2b686b856043277bf45c9970b1090722/' + latitude + ',' + longitude + '?units=si'
 
@@ -9,7 +11,7 @@ const forecast = (latitude, longitude,callback) => {
         } else if (body.error){
             callback('Unable to find location', undefined)
         } else {
-          callback(undefined, body.daily.data[0].summary  + 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+          callback(undefined, body.daily.data[0].summary  + 'The temperature high for today was ' + body.daily.data[0].temperatureHigh + ' degrees.' + ' The temperature low for today was ' + body.daily.data[0].temperatureLow + ' degrees. ' + 'It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
